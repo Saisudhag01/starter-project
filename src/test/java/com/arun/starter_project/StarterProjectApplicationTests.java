0f227cb06306f283e7753f03a36bbc2e8d132289
4RUN-30 <arunkumar061781@gmail.com>
2025-04-26 23:41:56 +0530

    Initial commit

---
+++ src/test/java/com/arun/starter_project/StarterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arun.starter_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
